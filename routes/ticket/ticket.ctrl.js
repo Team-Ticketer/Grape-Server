62f2c93f5f85d08b61da53ca53d056e8c86ce83a
WonJunKim <kimjnsjwj@gmail.com>
2018-10-28 04:37:14 +0900

    Update projection

--- routes/ticket/ticket.ctrl.js
+++ routes/ticket/ticket.ctrl.js
@@ -20,7 +20,7 @@ const buyTicket = function buyTicketWithWallet(req, res) {
 const getTicketList = async function getTicketListWithWallet(req, res) {
   const { walletAddress } = req.query;
   let tickets = await Ticket.find({ walletAddress, isUsed: false }, 'concertId ticketName walletAddress');
-  const concerts = await Concert.find({ contract: tickets.map(e => e.concertId) }, 'contract name placeid startDate');
+  const concerts = await Concert.find({ contract: tickets.map(e => e.concertId) }, 'contract name placeName startDate');
   tickets = tickets.map(t => Object.assign(t, concerts.find(c => c.contract === t.concertId)));
   res.status(200).json(tickets);
 };
