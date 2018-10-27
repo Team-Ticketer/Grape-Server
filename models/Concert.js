d298f48dd5ea60c87fc5d745cbd399754b9c28b3
WonJunKim <kimjnsjwj@gmail.com>
2018-10-27 11:03:42 +0900

    Update models

--- models/Concert.js
+++ models/Concert.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
 const Concert = new mongoose.Schema({
-
+  contract: String,
+  content: String,
+  lat: Number,
+  lng: Number,
 });
 
 module.exports = mongoose.model('Concert', Concert);
--- models/User.js
+++ models/User.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const User = new mongoose.Schema({
-
+  email: String,
+  phoneNum: String,
 });
 
 module.exports = mongoose.model('User', User);
