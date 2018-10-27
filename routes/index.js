1fc46d4ce6c913235ca12f254ee392266b974e6f
WonJunKim <kimjnsjwj@gmail.com>
2018-10-28 04:36:44 +0900

    Update route index.js

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router
-  .use('/concert', require('./concert'));
+  .use('/concert', require('./concert'))
+  .use('/ticket', require('./ticket'));
 
 module.exports = router;
