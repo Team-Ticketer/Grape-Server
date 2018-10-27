fd168d30eb375f4bb1e92d03a784afa2bfb47b6e
WonJunKim <kimjnsjwj@gmail.com>
2018-10-27 16:04:53 +0900

    Update Concert model

--- models/Concert.js
+++ models/Concert.js
@@ -2,10 +2,16 @@ const mongoose = require('mongoose');
 
 const Concert = new mongoose.Schema({
   contract: String,
+  name: String,
   content: String,
-  picture: 
+  picture: String,
   lat: Number,
   lng: Number,
+  startDate: Date,
+  endDate: Date,
+  ownerName: String,
+  minPrice: Number,
+  maxPrice: Number,
 });
 
 module.exports = mongoose.model('Concert', Concert);
