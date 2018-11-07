2c997f61386b785a84f265a294304a0af29ec684
WonJunKim <kimjnsjwj@gmail.com>
2018-11-07 23:17:26 +0900

    Add TODO & Add api for check whether wallet is registered

--- routes/user/index.js
+++ routes/user/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const ctrl = require('./user.ctrl');
 
-router.post('/register', ctrl.registerWallet)
+router
+  .get('/check', ctrl.checkWallet)
+  .post('/register', ctrl.registerWallet)
   .get('/nickname', ctrl.getNickname);
 
 module.exports = router;
--- routes/user/user.ctrl.js
+++ routes/user/user.ctrl.js
@@ -1,12 +1,31 @@
-const registerWallet = function registerWalletWithId(req, res) {
+const User = require('../../models/User');
 
+const checkWallet = function checkWalletWithKaKaoId(req, res) {
+  /*
+   * TODO:
+   * 1. id가 컬렉션에 존재하는지 확인
+   * 2. 있으면 True, 없으면 (등록해야하면) False
+   */
+};
+
+const registerWallet = function registerWalletWithId(req, res) {
+  /*
+   * TODO:
+   * 1. walletId이 중복되지 않았는지 확인
+   * 2. 저장
+   */
 };
 
 const getNickname = function getNicknameWithWalletId(req, res) {
-
+  /*
+   * TODO:
+   * 1. walletId를 배열로 받기
+   * 2. walletId에 해당하는 닉네임들 그대로 반환
+   */
 };
 
 module.exports = {
+  checkWallet,
   registerWallet,
   getNickname,
 };
