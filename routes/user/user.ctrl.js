8a486ac1aa65f4997cef7897d32e68b9398ef817
WonJunKim <kimjnsjwj@gmail.com>
2018-11-12 14:00:35 +0900

    Update user.ctrl.js

--- routes/user/user.ctrl.js
+++ routes/user/user.ctrl.js
@@ -6,22 +6,44 @@ const checkWallet = function checkWalletWithKaKaoId(req, res) {
    * 1. id가 컬렉션에 존재하는지 확인
    * 2. 있으면 True, 없으면 (등록해야하면) False
    */
+  const { kakaoId } = req.query;
+  User.findOne({ kakaoId })
+    .then((u) => {
+      if (u) res.status(200).json({ result: 'OK', isExist: true });
+      else res.status(200).json({ result: 'OK', isExist: false });
+    })
+    .catch((e) => {
+      console.log(e);
+      res.status(500).json({ result: 'failure' });
+    });
 };
 
-const registerWallet = function registerWalletWithId(req, res) {
+const registerWallet = async function registerWalletWithId(req, res) {
   /*
    * TODO:
    * 1. walletId이 중복되지 않았는지 확인
    * 2. 저장
    */
+  const { kakaoId, name, walletId } = req.body;
+  const u = await User.findOne({ walletId });
+
+  if (u) res.status(400).json({ result: 'failure', msg: 'Already Exist!' });
+  else {
+    const newUser = await (User({ kakaoId, name, walletId }).save());
+    console.log(newUser);
+    res.status(201).json({ result: 'OK' });
+  }
 };
 
-const getNickname = function getNicknameWithWalletId(req, res) {
+const getNickname = async function getNicknameWithWalletId(req, res) {
   /*
    * TODO:
    * 1. walletId를 배열로 받기
    * 2. walletId에 해당하는 닉네임들 그대로 반환
    */
+  const { walletIdArr } = req.body;
+  const names = await User.find({ walletId: { $in: walletIdArr } });
+  res.status(200).json(names);
 };
 
 module.exports = {
