3527f7890fa52c53995b754d94c1d2d898ae0162
WonJunKim <kimjnsjwj@gmail.com>
2018-11-13 23:19:24 +0900

    Update /ticket APIs

--- models/Ticket.js
+++ models/Ticket.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const Ticket = mongoose.Schema({
-  
+  walletId: String,
+  tickets: [String],
 });
 
 module.exports = mongoose.model('Ticket', Ticket);
--- routes/ticket/ticket.ctrl.js
+++ routes/ticket/ticket.ctrl.js
@@ -1,3 +1,24 @@
-module.exports = {
+const Ticket = require('../../models/Ticket');
 
-}
+// TODO: walletId로 가지고 있는 티켓의 정보 반환하기
+const getTickets = async function getTicketsWithWalletId(req, res) {
+  const { walletId } = req.query;
+  const { tickets } = await Ticket.findOne({ walletId });
+  res.status(200).json(tickets);
+};
+
+// TODO: 티켓 구매/양도와 같은 트랜잭션마다 티켓의 주인을 바꾸는 메소드, 파라미터에 이전 주인 walletId로 추적
+const registerOwner = function registerOwnerWithWalletId(req, res) {
+  const { before, walletId, concertId } = req.body;
+  Promise.all([
+    Ticket.findOneAndUpdate({ walletId: before }, { $pull: { tickets: concertId } }),
+    Ticket.findOneAndUpdate({ walletId }, { $push: { tickets: concertId } }),
+  ]).then(() => {
+    res.status(201).json({ result: 'OK' });
+  });
+};
+
+module.exports = {
+  getTickets,
+  registerOwner,
+};
