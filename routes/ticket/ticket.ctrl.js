a311057000185b7576c9f91c623b644e42d09ebb
WonJunKim <kimjnsjwj@gmail.com>
2018-11-19 19:30:40 +0900

    Modify code from object destructuring to projection

--- routes/ticket/ticket.ctrl.js
+++ routes/ticket/ticket.ctrl.js
@@ -3,7 +3,7 @@ const Ticket = require('../../models/Ticket');
 // TODO: walletId로 가지고 있는 티켓의 정보 반환하기
 const getTickets = async function getTicketsWithWalletId(req, res) {
   const { walletId } = req.query;
-  const { tickets } = await Ticket.findOne({ walletId });
+  const tickets = await Ticket.findOne({ walletId }, 'tickets');
   res.status(200).json(tickets);
 };
 
